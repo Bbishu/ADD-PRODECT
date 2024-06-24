82734d5e2b791d92f18ac3d3e0d06e307a2a7bfc
Bishwajeet Malik <bishwajeet.techmarbles@gmail.com>
2024-06-24 15:35:23 +0530

    Create header.php

---
+++ images/header.php
@@ -0,0 +1,19 @@
+<?php require_once 'connect.php';
+
+?>
+<html lang="en">
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+  <title>Form </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+ 
